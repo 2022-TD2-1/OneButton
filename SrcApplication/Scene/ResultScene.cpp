b69fa4b27c4405685d019c7ff8497170a1c7a738
LC1A_09_suzuki <k021g1127@g.neec.ac.jp>
2022-10-15 14:44:17 +0900

    クリアタイムを表示できるように

--- SrcApplication/Scene/ResultScene.cpp
+++ SrcApplication/Scene/ResultScene.cpp
@@ -2,11 +2,7 @@
 
 void ResultScene::Init()
 {
-	//テスト用画像読み込み
-	//testSprite = Sprite("test");
-	testSprite = Sprite("Resources/one.png"	,"test");
-	testSprite.position = { 128,128,0 };
-
+#pragma region 数字読み込み
 	numSprite[0] = Sprite("Resources/zero.png"	, "zero");
 	numSprite[1] = Sprite("Resources/one.png"	, "one");
 	numSprite[2] = Sprite("Resources/two.png"	, "two");
@@ -17,26 +13,36 @@ void ResultScene::Init()
 	numSprite[7] = Sprite("Resources/seven.png"	, "seven");
 	numSprite[8] = Sprite("Resources/eight.png"	, "eight");
 	numSprite[9] = Sprite("Resources/nine.png"	, "nine");
-
-	//testnumSprite = Sprite("Resources/zero.png", "zero");
-
-	for (int i = 0; i < 10; i++) {
-		numSprite[i].position.x = 50 + 50 * i;
-		numSprite[i].position.y = 50;
-		numSprite[i].position.z = 0;
-	}
-	//testnumSprite.position = { 50,50,0 };
-
+	dotSprite = Sprite("Resources/point.png", "point");
+#pragma endregion
 	
+	//時間を代入する
+	int time = (testTime * 100);
+	displayNum[0] = (time % 10000) / 1000;
+	displayNum[1] = (time % 1000) / 100;
+	displayNum[2] = (time % 100) / 10;
+	displayNum[3] = (time % 10);
+
+	const int display[5] =
+	{ displayNum[0],displayNum[1],0, displayNum[2], displayNum[3] };
+	for (int i = 0; i < 5; i++) {
+		if (i == 2) {
+			//小数点の座標を代入
+			dotSprite.position.x = 50 + (50 * i);
+			dotSprite.position.y = 50;
+			dotSprite.position.z = 0;
+		}
+		else {
+			//数字の座標を代入
+			numSprite[display[i]].position.x = 50 + (50 * i);
+			numSprite[display[i]].position.y = 50;
+			numSprite[display[i]].position.z = 0;
+		}
+	}
 }
 
 void ResultScene::Update()
 {
-	testSprite.UpdateMatrix();
-
-	for (int i = 0; i < 10; i++) {
-		numSprite[i].UpdateMatrix();
-	}
 	//メニューセレクト
 	if (Input::Key::Triggered(DIK_UP))
 	{
@@ -62,7 +68,12 @@ void ResultScene::Update()
 		}
 	}
 
-
+	//スプライト更新
+	for (int i = 0; i < 10; i++) {
+		numSprite[i].UpdateMatrix();
+	}
+	dotSprite.UpdateMatrix();
+	
 }
 
 void ResultScene::DrawBack()
@@ -75,11 +86,14 @@ void ResultScene::Draw3D()
 
 void ResultScene::DrawSprite()
 {
-	//testSprite.Draw();
-
-	//numSprite[0].Draw();
-
-	for (int i = 0; i < 10; i++) {
-		numSprite[i].Draw();
+	
+	const int display[4] =
+	{ displayNum[0],displayNum[1], displayNum[2], displayNum[3], };
+	for (int i = 0; i < 4; i++) {
+		//数字を描画
+		numSprite[display[i]].Draw();
 	}
+	//小数点を描画
+	dotSprite.Draw();
+	
 }
