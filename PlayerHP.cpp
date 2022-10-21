7daa7ccd130f27466ef7a9d79a04ae0c986211ee
Desctop <User@DESKTOP-FAGQ0KA>
2022-10-21 16:42:22 +0900

    プレイヤーのHPクラスを追加しただけ。ばいといってきまーーーーー

---
+++ PlayerHP.cpp
@@ -0,0 +1,13 @@
+#include "PlayerHP.h"
+
+void PlayerHP::Ini()
+{
+}
+
+void PlayerHP::Update()
+{
+}
+
+void PlayerHP::Draw()
+{
+}
---
+++ PlayerHP.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Object3D.h>
+class PlayerHP
+{
+public:
+	void Ini();
+	void Update();
+	void Draw();
+private:
+	Object3D hpObj;
+};
+
