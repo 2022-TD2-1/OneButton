5542cf14ea42d97d0fa898736efada6a5ba71d97
Desctop <User@DESKTOP-FAGQ0KA>
2022-10-25 00:15:12 +0900

    カウントのサイズ調整

--- CountDown.cpp
+++ CountDown.cpp
@@ -5,22 +5,22 @@ void CountDown::Ini()
 {
 	numberObj[0].model = ModelManager::Get("Start");
 	numberObj[0].position = { 0,0,-5 };
-	numberObj[0].scale = { 2,2,2 };
+	numberObj[0].scale = { 3,3,3 };
 	numberObj[0].UpdateMatrix();
 
 	numberObj[1].model = ModelManager::Get("One");
 	numberObj[1].position = { 0,0,-5 };
-	numberObj[1].scale = { 2,2,2 };
+	numberObj[1].scale = { 3,3,3 };
 	numberObj[1].UpdateMatrix();
 
 	numberObj[2].model = ModelManager::Get("Two");
 	numberObj[2].position = { 0,0,-5 };
-	numberObj[2].scale = { 2,2,2 };
+	numberObj[2].scale = { 3,3,3 };
 	numberObj[2].UpdateMatrix();
 
 	numberObj[3].model = ModelManager::Get("Three");
 	numberObj[3].position = { 0,0,-5 };
-	numberObj[3].scale = { 2,2,2 };
+	numberObj[3].scale = { 3,3,3 };
 	numberObj[3].UpdateMatrix();
 
 }
